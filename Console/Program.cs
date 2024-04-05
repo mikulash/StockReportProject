ba9ef4e66afbb7a48efd12d3bdb7ac003e81e6b9
Ondřej Zelinka <xzelinka@fi.muni.cz>
2024-04-05 20:03:41 +0200

    STO-15 download csv, store to file, argument check

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,21 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using FileLoader.FileParserStrategy;
-using FileLoader.Reader;
-using DiffCalculator.IndexRecordDiffCalculator;
-using DiffCalculator.Model;
-using DiffCalculator.Positions.Visitor;
-
-// TODO: write the actual paths
-var fileReaderA = new FileReader("/path/to/fileA.csv");
-var fileReaderB = new FileReader("/path/to/fileB.csv");
-var parser = new ParserMiddleware();
-        
-var recordListA = parser.ParseFileToList(fileReaderA);
-var recordListB = parser.ParseFileToList(fileReaderB);
-
-var diffCalc = new IndexRecordListDiffCalculator(recordListA, recordListB);
-var recordDiffs = diffCalc.GetIndexRecordListDiff();
-List<IVisitor> visitors = new List<IVisitor>()
-    { new NewPositionsVisitor(), new IncreasedPositionsVisitor(), new DecreasedPositionsVisitor() };
-visitors.ForEach(visitor => visitor.Visit(recordDiffs));
+﻿// using Common;
+// using WebScraping;
+//
+// <<<<<<< HEAD
+// using FileLoader.FileParserStrategy;
+// using FileLoader.Reader;
+// using DiffCalculator.IndexRecordDiffCalculator;
+// using DiffCalculator.Model;
+// using DiffCalculator.Positions.Visitor;
+//
+// // TODO: write the actual paths
+// var fileReaderA = new FileReader("/path/to/fileA.csv");
+// var fileReaderB = new FileReader("/path/to/fileB.csv");
+// var parser = new ParserMiddleware();
+//         
+// var recordListA = parser.ParseFileToList(fileReaderA);
+// var recordListB = parser.ParseFileToList(fileReaderB);
+//
+// var diffCalc = new IndexRecordListDiffCalculator(recordListA, recordListB);
+// var recordDiffs = diffCalc.GetIndexRecordListDiff();
+// List<IVisitor> visitors = new List<IVisitor>()
+//     { new NewPositionsVisitor(), new IncreasedPositionsVisitor(), new DecreasedPositionsVisitor() };
+// visitors.ForEach(visitor => visitor.Visit(recordDiffs));
+// =======
+// namespace Console;
+// class Program
+// {
+//     static async Task Main(string[] args)
+//     {
+//         System.Console.WriteLine("Welcome to Stock Report App!");
+//
+//         // arg check and setup
+//         var argumentCheck = new ArgumentCheck();
+//         var arguments = argumentCheck.Setup(args);
+//
+//         // File download
+//         if (arguments.DownloadFrequency == Frequency.Once)
+//             await DownloadOnce(arguments.WebFilePath, arguments.FilePathName);
+//     }
+//
+//     private static async Task DownloadOnce(string baseWebFile, string filePath)
+//     {
+//         var webFile = new WebFile(baseWebFile);
+//         var response = await webFile.DownloadAndSaveTo(filePath);
+//         System.Console.WriteLine(response.success
+//             ? $"File was successfully downloaded and saved to {filePath}"
+//             : $"Error: {response.msg}");
+//     }
+// }
+// >>>>>>> 702a648 (STO-15 download csv, store to file, argument check)
---
+++ FileManager/FileManager.cs
@@ -0,0 +1,34 @@
+﻿namespace FileManager;
+
+public class FileManager
+{
+    // Returns true on successful stream write to specified file
+    public async Task<(bool success, string msg)> SaveStreamToFile(Stream stream, string filePath)
+    {
+        if (!ValidateFilePath(filePath))
+            return (false, $"Invalid file path {filePath}");
+        
+        try
+        {
+            await using var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
+            await stream.CopyToAsync(fileStream);
+
+            return (true, String.Empty);
+        }
+        catch (Exception ex)
+        {
+            return (false, $"Problem while creating a file. Exception: {ex.Message}");
+        }
+    }
+
+    public string GetStockCsvFileNameForDate(DateTime dateTime)
+    {
+        return $"stock-{dateTime:yyyy-MM-dd}.csv";
+    }
+
+    // Returns true when path exists
+    private bool ValidateFilePath(string filePath)
+    {
+        return Path.GetDirectoryName(filePath) != null;
+    }
+}
