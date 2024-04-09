5947d246df9e4d0cf59a01072212f5ab177df7db
Daniel Havranek <xhavran3@fi.muni.cz>
2024-04-09 09:03:16 +0200

    feat: example usage of DiffCalculator

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using FileLoader.FileParserStrategy;
+using FileLoader.Reader;
+using DiffCalculator.IndexRecordDiffCalculator;
+
+// TODO: write the actual paths
+var fileReaderA = new FileReader("/path/to/fileA.csv");
+var fileReaderB = new FileReader("/path/to/fileB.csv");
+var parser = new ParserMiddleware();
+        
+var recordListA = parser.ParseFileToList(fileReaderA);
+var recordListB = parser.ParseFileToList(fileReaderB);
+
+var diffCalc = new IndexRecordListDiffCalculator();
+var diffList = diffCalc.GetIndexRecordListDiff(recordListA, recordListB);
+
+foreach (var item in diffList) {
+    Console.WriteLine(item.Company + " " + item.SharesDiff);
+}
