b0f5b809066bb54b4d6b54c97d3176948f8dbcd6
Jozef Mihale <xmihale@fi.muni.cz>
2024-04-10 16:33:28 +0200

    feat: Creating Decorator pattern for filters and fixing issue with Converter

--- FileLoader/FileParserStrategy/CommaSeparatedValues/Converters/SpecialCharacterRemovalConverter.cs
+++ FileLoader/FileParserStrategy/CommaSeparatedValues/Converters/SpecialCharacterRemovalConverter.cs
@@ -10,9 +10,9 @@ public class SpecialCharacterRemovalConverter : DefaultTypeConverter
 {
     private readonly char[] _charactersToExclude = [',', '$', '%'];
     
-    public override object? ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    public override object? ConvertFromString(string? text, IReaderRow row, MemberMapData memberMapData)
     {
-        string clean = text.ExcludeCharacters(_charactersToExclude);
+        string clean = (text ?? string.Empty).ExcludeCharacters(_charactersToExclude);
 
         return double.TryParse(clean, CultureInfo.InvariantCulture, out double result) ? result : null;
     }
