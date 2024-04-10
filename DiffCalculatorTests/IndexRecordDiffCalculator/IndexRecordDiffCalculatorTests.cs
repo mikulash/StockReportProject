d1210549d3d58c7e063408570678b732c4292b81
David Rusnak <davidkorusnak@gmail.com>
2024-04-10 13:17:59 +0200

    feat: tests

--- DiffCalculatorTests/IndexRecordDiffCalculator/IndexRecordDiffCalculatorTests.cs
+++ DiffCalculatorTests/IndexRecordDiffCalculator/IndexRecordDiffCalculatorTests.cs
@@ -6,7 +6,6 @@ namespace DiffCalculatorTests.IndexRecordDiffCalculator;
 
 public class IndexRecordDiffCalculatorTests
 {
-
     [SetUp]
     public void Setup()
     {
@@ -144,6 +143,7 @@ public class IndexRecordDiffCalculatorTests
                     Ticker = "Test Ticker 1",
                     DayDiff = 0,
                     SharesDiff = 0,
+                    SharesDiffPercentage = 0,
                     MarketValueDiff = 0,
                     WeightDiff = 0,
                     IsNew = false
@@ -204,6 +204,7 @@ public class IndexRecordDiffCalculatorTests
                     Ticker = "Test Ticker 1",
                     DayDiff = 1,
                     SharesDiff = 500,
+                    SharesDiffPercentage = 50,
                     MarketValueDiff = 200,
                     WeightDiff = -100,
                     IsNew = false
@@ -218,6 +219,66 @@ public class IndexRecordDiffCalculatorTests
         AssertIndexRecordDiffListsAreEqual(expected.DiffRecords, actual.DiffRecords);
     }
 
+    [Test]
+    public void GetIndexRecordListDiff_Decrease_ReturnsNegativeValue()
+    {
+        // Arrange
+        var listA = new List<IndexRecordDto>()
+        {
+            new IndexRecordDto
+            {
+                CUSIP = "A1",
+                Company = "Test Company 1",
+                Fund = "Test Fund 1",
+                Ticker = "Test Ticker 1",
+                Date = new DateOnly(2024, 1, 1),
+                MarketValue = 1000,
+                Shares = 1500,
+                Weight = 1000
+            },
+        };
+        var listB = new List<IndexRecordDto>()
+        {
+            new IndexRecordDto
+            {
+                CUSIP = "A1",
+                Company = "Test Company 1",
+                Fund = "Test Fund 1",
+                Ticker = "Test Ticker 1",
+                Date = new DateOnly(2024, 1, 2),
+                MarketValue = 1200,
+                Shares = 1000,
+                Weight = 900
+            },
+        };
+        var diffCalculator = new IndexRecordListDiffCalculator(listA, listB);
+
+        var expected = new RecordDiffs()
+        {
+            DiffRecords = new List<IndexRecordDiffDto>()
+            {
+                new IndexRecordDiffDto()
+                {
+                    CUSIP = "A1",
+                    Company = "Test Company 1",
+                    Fund = "Test Fund 1",
+                    Ticker = "Test Ticker 1",
+                    DayDiff = 1,
+                    SharesDiff = -500,
+                    SharesDiffPercentage = -(1.0 / 3) * 100,
+                    MarketValueDiff = 200,
+                    WeightDiff = -100,
+                    IsNew = false
+                }
+            }
+        };
+
+        // Act
+        var actual = diffCalculator.GetIndexRecordListDiff();
+
+        // Assert
+        AssertIndexRecordDiffListsAreEqual(expected.DiffRecords, actual.DiffRecords);
+    }
 
     private void AssertIndexRecordDiffsAreEqual(IndexRecordDiffDto a, IndexRecordDiffDto b)
     {
@@ -228,18 +289,18 @@ public class IndexRecordDiffCalculatorTests
         Assert.That(a.DayDiff, Is.EqualTo(b.DayDiff));
         Assert.That(a.MarketValueDiff, Is.EqualTo(b.MarketValueDiff));
         Assert.That(a.SharesDiff, Is.EqualTo(b.SharesDiff));
+        Assert.That(a.SharesDiffPercentage, Is.EqualTo(b.SharesDiffPercentage));
         Assert.That(a.WeightDiff, Is.EqualTo(b.WeightDiff));
         Assert.That(a.IsNew, Is.EqualTo(b.IsNew));
     }
-    
+
     private void AssertIndexRecordDiffListsAreEqual(List<IndexRecordDiffDto> a, List<IndexRecordDiffDto> b)
     {
         Assert.That(a.Count, Is.EqualTo(b.Count));
-        
-        for(var i = 0; i < a.Count; i++)
+
+        for (var i = 0; i < a.Count; i++)
         {
             AssertIndexRecordDiffsAreEqual(a[i], b[i]);
         }
     }
-
 }
