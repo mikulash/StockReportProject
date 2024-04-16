f69ac1546977a6e7eaa009ab85ce43d0e03b0289
Mikulas Heinz <483813@muni.cz>
2024-04-16 23:01:59 +0200

    azure scraper init

---
+++ Scraper/Scraper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Scraper;
+
+public class Scraper
+{
+    private readonly ILogger _logger;
+
+    public Scraper(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<Scraper>();
+    }
+
+    [Function("Scraper")]
+    public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+    {
+        _logger.LogInformation("C# Timer trigger function executed at: {Now}", DateTime.Now);
+
+        if (myTimer.ScheduleStatus is not null)
+        {
+            _logger.LogInformation("Next timer schedule at: {ScheduleStatusNext}", myTimer.ScheduleStatus.Next);
+        }
+
+    }
+}
