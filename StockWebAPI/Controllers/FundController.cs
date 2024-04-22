03c51fb441ec8bbaa70733047fedb97455af1ef0
Jozef Mihale <xmihale@fi.muni.cz>
2024-04-22 11:13:17 +0200

    fix: Class field name change

--- StockWebAPI/Controllers/FundController.cs
+++ StockWebAPI/Controllers/FundController.cs
@@ -14,17 +14,17 @@ namespace StockWebAPI.Controllers;
 [Route("api/[controller]")]
 public class FundController : ControllerBase
 {
-    private IGenericFacade<Fund, long, CreateFundDto, UpdateFundDto, ViewFundDto, ViewFundDto, FundFilter> _facade;
+    private IGenericFacade<Fund, long, CreateFundDto, UpdateFundDto, ViewFundDto, ViewFundDto, FundFilter> _fundFacade;
 
-    public FundController(IGenericFacade<Fund, long, CreateFundDto, UpdateFundDto, ViewFundDto, ViewFundDto, FundFilter> facade)
+    public FundController(IGenericFacade<Fund, long, CreateFundDto, UpdateFundDto, ViewFundDto, ViewFundDto, FundFilter> fundFacade)
     {
-        _facade = facade;
+        _fundFacade = fundFacade;
     }
     
     [HttpPost]
     public async Task<IActionResult> CreateFund(CreateFundDto createFundDto)
     {
-        var fund = await _facade.CreateAsync(createFundDto);
+        var fund = await _fundFacade.CreateAsync(createFundDto);
         return Created(
             new Uri($"{Request.Path}/{fund.Id}", UriKind.Relative),
             fund
@@ -32,27 +32,27 @@ public class FundController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<IActionResult> FetchAll() => Ok(await _facade.FetchAllAsync());
+    public async Task<IActionResult> FetchAll() => Ok(await _fundFacade.FetchAllAsync());
     
     [HttpGet]
     [Route("filter")]
     public async Task<IActionResult> FetchAllFiltered([FromQuery] FundFilterDto filter) 
-        => Ok(await _facade.FetchAllFilteredAsync(filter));
+        => Ok(await _fundFacade.FetchAllFilteredAsync(filter));
 
     [HttpPut]
     [Route("{id}")]
     public async Task<IActionResult> UpdateFund(long id, UpdateFundDto updateFundDto) 
-        => Ok(await _facade.UpdateAsync(id, updateFundDto));
+        => Ok(await _fundFacade.UpdateAsync(id, updateFundDto));
 
     [HttpGet]
     [Route("{id}")]
-    public async Task<IActionResult> FindById(long id) => Ok(await _facade.FindByIdAsync(id));
+    public async Task<IActionResult> FindById(long id) => Ok(await _fundFacade.FindByIdAsync(id));
 
     [HttpDelete]
     [Route("{id}")]
     public async Task<IActionResult> DeleteById(long id)
     {
-        await _facade.DeleteByIdAsync(id);
+        await _fundFacade.DeleteByIdAsync(id);
         return NoContent();
     }
 }
