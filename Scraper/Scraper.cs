a5ef626023c0c6eb7840c71b994c5f9e146cb032
Mikulas Heinz <483813@muni.cz>
2024-05-03 21:13:06 +0200

    feat: csv downloadable on headless

--- Scraper/Scraper.cs
+++ Scraper/Scraper.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace Scraper;
 
@@ -17,7 +18,9 @@ public class Scraper
     }
 
     [Function("Scraper")]
-    public void Run([TimerTrigger("0 0 2 * * *")] TimerInfo myTimer)
+    // public void Run([TimerTrigger("0 0 2 * * *")] TimerInfo myTimer)
+
+    public void Run([TimerTrigger("0 * * * * *")] TimerInfo myTimer)
     {
         _logger.LogInformation("C# Timer trigger function executed at: {Now}", DateTime.Now);
         Scrape().Wait();
@@ -25,27 +28,35 @@ public class Scraper
 
     private static async Task Scrape()
     {
+        IWebDriver? driver = null;
         try
         {
-            // var chromeOptions = new ChromeOptions();
-            // chromeOptions.AddArgument("--headless");
-            var driver = new ChromeDriver();
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+            chromeOptions.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36");
+            driver = new ChromeDriver(chromeOptions);
+            // driver = new ChromeDriver();
             driver.Navigate().GoToUrl(url);
 
             //agree cookies
             driver.FindElement(By.XPath("//*[@id=\"agree_button\"]")).Click();
             driver.FindElement(By.XPath("//*[@id=\"hs-eu-confirmation-button\"]")).Click();
             // select document type
-            driver.FindElement(By.XPath("/html/body/div[5]/div[2]/div[2]/div/div/div/div/div[2]/div[1]/div/ul/li[8]/a")).Click();
+            IWebElement element = driver.FindElement(By.XPath("/html/body/div[5]/div[2]/div[2]/div/div/div/div/div[2]/div[1]/div/ul/li[8]/a"));
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", element);
+            element.Click();
+            // driver.FindElement(By.XPath("/html/body/div[5]/div[2]/div[2]/div/div/div/div/div[2]/div[1]/div/ul/li[8]/a")).Click();
             // select document
             var downloadLink = driver.FindElement(By.XPath("//*[@id=\"doc-1793\"]/a")).GetAttribute("href");
             driver.Close();
             await DownloadFileAsync(downloadLink);
+            Console.WriteLine("File downloaded successfully!");
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error: {ex.Message}");
         }
+
     }
 
     private static async Task DownloadFileAsync(string fileUrl)
