5da70aa6584212d6369f9f8a3befd6701d513d95
Jozef Mihale <xmihale@fi.muni.cz>
2024-05-07 13:06:18 +0200

    feat: Adding infrastructure tests

---
+++ InfrastructureTests/Fund/FundRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using GenericDataAccessLayer.DatabaseTypeStrategy;
+using GenericDataAccessLayer.DatabaseTypeStrategy.DatabaseStrategies;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace InfrastructureTests.Fund;
+
+public class FundRepositoryTests
+{
+    public IConfiguration CreateConfig()
+    {
+        var mockDbContext = new Mock<IConfiguration>();
+        mockDbContext
+            .Setup(mock => mock.GetSection(EnumToStrategyConverter.AppSettingsKeyName).Value)
+            .Returns(DatabaseType.InMemory.ToString());
+        
+        return mockDbContext.Object;
+    }
+}
