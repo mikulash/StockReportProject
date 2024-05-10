4ff6297ff3c13c175deae39deb46349719eb239b
Daniel Havranek <daniel7861@mailbox.org>
2024-05-10 12:37:10 +0200

    feat: add dashboard page

--- WebStockClient/src/Router.tsx
+++ WebStockClient/src/Router.tsx
@@ -1,12 +1,18 @@
 import { useRoutes } from "react-router-dom";
 import CatFactPage from "./pages/CatFactPage";
 import Error404Page from "./pages/Error404Page";
+import DashboardPage from "./pages/DashboardPage";
 
 const Router = () => {
   return useRoutes([
     {
       path: `/`,
       element: <CatFactPage />,
+      errorElement: <Error404Page />,
+    },
+    {
+      path: `/dashboard`,
+      element: <DashboardPage />,
       errorElement: <Error404Page />,
     },
     {
