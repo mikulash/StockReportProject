e89371a5717bb1ccec0377cb6416ddf28a570fff
Daniel Havranek <daniel7861@mailbox.org>
2024-05-11 12:01:30 +0200

    feat: handle errors for email subscribtion

--- WebStockClient/src/pages/SubscriptionPage.tsx
+++ WebStockClient/src/pages/SubscriptionPage.tsx
@@ -7,6 +7,7 @@ import { MailSubscriberSchema } from "../schema/mailSubscriberSchema";
 
 const SubscribtionPage: FunctionComponent = () => {
   const [showSuccessToast, setShowSuccessToast] = useState(false);
+  const [showErrorToast, setShowErrorToast] = useState(false);
   const {
     register,
     handleSubmit,
@@ -16,9 +17,16 @@ const SubscribtionPage: FunctionComponent = () => {
   const queryPost = useMailSubscriberCreate();
 
   const onSubmit: SubmitHandler<MailSubscriber> = async (data) => {
-    await queryPost.mutateAsync(data);
+    setShowErrorToast(false);
+    setShowSuccessToast(false);
+    try {
+      await queryPost.mutateAsync(data);
+    } catch {
+      setShowErrorToast(true);
+      return;
+    }
     if (queryPost.isError) {
-      console.log(queryPost.error);
+      setShowErrorToast(true);
     } else {
       resetField("email");
       setShowSuccessToast(true);
@@ -76,15 +84,81 @@ const SubscribtionPage: FunctionComponent = () => {
                 </button>
               </div>
             </div>
+
             {errors.email && (
-              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-                <span className="font-medium">{errors.email?.message}</span>
-              </p>
+              <div
+                className="flex mx-auto max-w-screen-sm items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+                role="alert"
+              >
+                <svg
+                  className="flex-shrink-0 inline w-4 h-4 me-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span className="sr-only">Info</span>
+                <div>
+                  <span className="font-medium">{errors.email?.message}</span>
+                </div>
+              </div>
             )}
             <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">
               We care about the protection of your data.
             </div>
           </form>
+
+          {showErrorToast && (
+            <div className="py-12 flex justify-center">
+              <div
+                id="toast-danger"
+                className="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+                role="alert"
+              >
+                <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" />
+                  </svg>
+                  <span className="sr-only">Error icon</span>
+                </div>
+                <div className="ms-3 text-sm font-normal">
+                  Unable to subscribe.
+                </div>
+                <button
+                  type="button"
+                  className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+                  data-dismiss-target="#toast-danger"
+                  aria-label="Close"
+                  onClick={() => setShowErrorToast(false)}
+                >
+                  <span className="sr-only">Close</span>
+                  <svg
+                    className="w-3 h-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 14"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          )}
           {showSuccessToast && (
             <div className="py-12 flex justify-center">
               <div
