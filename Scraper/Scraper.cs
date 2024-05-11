12d1a515b42246a54a87963070a87b1e775a6f74
Mikulas Heinz <483813@muni.cz>
2024-05-11 19:07:31 +0200

    feat: remove commented code

--- Scraper/Scraper.cs
+++ Scraper/Scraper.cs
@@ -34,7 +34,6 @@ public class Scraper
             chromeOptions.AddArgument("--headless");
             chromeOptions.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36");
             driver = new ChromeDriver(chromeOptions);
-            // driver = new ChromeDriver();
             driver.Navigate().GoToUrl(url);
 
             //agree cookies
