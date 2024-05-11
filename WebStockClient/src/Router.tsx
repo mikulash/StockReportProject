fae71aee7b26329193fc6a87be990e6ecb730603
Daniel Havranek <daniel7861@mailbox.org>
2024-05-11 13:41:45 +0200

    feat: subscription page as a homepage

--- WebStockClient/src/Router.tsx
+++ WebStockClient/src/Router.tsx
@@ -1,5 +1,4 @@
 import { useRoutes } from "react-router-dom";
-import CatFactPage from "./pages/CatFactPage";
 import Error404Page from "./pages/Error404Page";
 import EmailManagementPage from "./pages/Dashboard/EmailManagementPage";
 import SubscribtionPage from "./pages/SubscriptionPage";
@@ -8,17 +7,12 @@ const Router = () => {
   return useRoutes([
     {
       path: `/`,
-      element: <CatFactPage />,
+      element: <SubscribtionPage />,
       errorElement: <Error404Page />,
     },
     {
       path: `/dashboard`,
       element: <EmailManagementPage />,
-      errorElement: <Error404Page />,
-    },
-    {
-      path: `/subscribe`,
-      element: <SubscribtionPage />,
       errorElement: <Error404Page />,
     },
     { path: "*", element: <Error404Page /> },
--- WebStockClient/src/api/catFacts.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import api from ".";
-
-export const useCatFact = () =>
-  useQuery({
-    queryKey: ["get", "fact"],
-    queryFn: async () =>
-      (await api().get("/cat-api/fact").json()) as { fact: string },
-  });
