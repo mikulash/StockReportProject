39cb22b6109f82f11562877bbbdfefb02799dd86
Daniel Havranek <daniel7861@mailbox.org>
2024-05-14 20:17:59 +0200

    feat: updated mail subscriber schema

--- WebStockClient/src/model/mailSubscriber.ts
+++ WebStockClient/src/model/mailSubscriber.ts
@@ -1,6 +1,7 @@
-export type MailSubscriber = {
-  email: string;
-};
+import { z } from "zod";
+import { MailSubscriberSchema } from "../schema/mailSubscriberSchema";
+
+export type MailSubscriber = z.infer<typeof MailSubscriberSchema>;
 
 export type MailSubscriberDetail = MailSubscriber & {
   id: number;
--- WebStockClient/src/schema/mailSubscriberSchema.ts
+++ WebStockClient/src/schema/mailSubscriberSchema.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
 
+const MailSubscriberPreferenceSchema = z.object({
+  fundName: z.string().max(200),
+  outputType: z.enum(["html", "string"]),
+});
+
 export const MailSubscriberSchema = z.object({
   email: z.string().email().max(200),
+  preferences: z.array(MailSubscriberPreferenceSchema).min(1),
 });
